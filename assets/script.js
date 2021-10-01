412c47b89336da84c42cec7d5d2c97edb537d2c1
Cristiano Rodrigues <vladrodrigues2003@gmail.com>
2021-10-01 08:13:19 +0100

    Seção de equipamentos

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,27 @@ const inputBar = document.querySelector("#input-bar");
 
 searchBtn.addEventListener("click",
   () => inputBar.classList.toggle("visible"));
+
+/* Slide header */
+const headerElt = document.querySelector("#slide-header");
+const bodySections = document.querySelectorAll(".slide-body .section");
+const linksElt = headerElt.querySelectorAll("a");
+const links = Array.from(linksElt);
+
+links.forEach(link => {
+  function diselect(elt) { elt.classList.remove("selected"); };
+  function hide(elt) { elt.classList.remove("visible"); }
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    links.forEach(diselect);
+    link.classList.add("selected");
+
+    const targetId = link.getAttribute("data-target");
+    const targetElt = document.querySelector(`#${targetId}`);
+
+    if (!targetElt) return;
+
+    Array.from(bodySections).forEach(hide);
+    targetElt.classList.add("visible");
+  })
+});
